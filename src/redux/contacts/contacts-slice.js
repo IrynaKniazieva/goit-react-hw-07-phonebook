a692eaec91fabe97205be674aaf8a0bdf8989949
IrynaKniazieva <gleb0812@gmail.com>
2023-03-06 22:21:37 +0200

    Update contacts-slice

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -1,16 +1,54 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import {fetchAllContactsLoading, fetchAllContactsSuccess, fetchAllContactsError, fetchAddContactLoading, fetchAddContactSuccess, fetchAddContactError, fetchDeleteContactLoading, fetchDeleteContactSuccess, fetchDeleteContactError} from "./contacts-actions";
+
+const initialState = {
+    items: [],
+    loading: false,
+    error: null,
+}
+
 const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: [],
-  reducers: {
-    addContact: (state, { payload }) => {
-      state.push(payload);
-    },
-    deleteContact: (state, { payload }) =>
-      state.filter(({ id }) => id !== payload),
-  },
-});
+    name: "contacts",
+    initialState,
+    extraReducers: {
+        [fetchAllContactsLoading]: (store) => {
+            store.loading = true;
+        },
+        [fetchAllContactsSuccess]: (store, {payload}) => {
+            console.log(payload);
+            store.loading = false;
+            store.items = payload;
+        },
+        [fetchAllContactsError]: (store, {payload}) => {
+            store.loading = false;
+            store.error = payload;
+        },
+        [fetchAddContactLoading]: (store) => {
+            store.loading = true;
+        },
+        [fetchAddContactSuccess]: (store, {payload}) => {
+            store.loading = false;
+            store.items.push(payload);
+        },
+        [fetchAddContactError]: (store, {payload}) => {
+            store.loading = false;
+            store.error = payload;
+        },
+        [fetchDeleteContactLoading]: (store) => {
+            store.loading = true;
+        },
+        [fetchDeleteContactSuccess]: (store, {payload}) => {
+            store.loading = false;
+            const index = store.items.findIndex(item => item.id === payload);
+            store.items.splice(index, 1);
+        },
+        [fetchDeleteContactError]: (store, {payload}) => {
+            store.loading = false;
+            store.error = payload;
+        }
+    }
+})
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export default contactsSlice.reducer;
