be83cec018f3db26c6c9132a546eddfab59ef7aa
IrynaKniazieva <gleb0812@gmail.com>
2023-03-06 22:27:31 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 import ContactsBook from 'components/ContactsBook/ContactsBook';
 
-import { store, persistor } from 'redux/store';
+import store from 'redux/store';
+
+// import { store, persistor } from 'redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
           <ContactsBook />
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   );
 };
